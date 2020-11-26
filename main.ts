6b2c3ca933c83795f3f93e009a4656e86f4457b9
hikerguy1900 <ericb@piap.com>
2020-11-25 18:09:42 -0800

    Copied JavaScript program from non GitHub project into GitHub.

--- main.ts
+++ main.ts
@@ -1,5 +1,40 @@
+input.buttonA.onEvent(ButtonEvent.LongClick, function () {
+    music.setVolume(255)
+    music.playMelody("A F E F D G E F ", 120)
+    music.setVolume(0)
+})
+input.buttonA.onEvent(ButtonEvent.Click, function () {
+    button_count += 1
+    if (button_count >= 6) {
+        button_count = 0
+    }
+})
+let button_count = 0
+button_count = 0
+music.stopAllSounds()
+music.setVolume(0)
 forever(function () {
-    light.showRing(
-    "red red red red red red red red red red"
-    )
+    if (button_count == 1) {
+        light.showRing(
+        `black black black pink pink black black black black black`
+        )
+    } else if (button_count == 2) {
+        light.showRing(
+        `pink pink black pink pink black black black black black`
+        )
+    } else if (button_count == 3) {
+        light.showRing(
+        `pink pink black pink pink black black black purple purple`
+        )
+    } else if (button_count == 4) {
+        light.showRing(
+        `pink pink black pink pink pink pink black purple purple`
+        )
+    } else if (button_count == 5) {
+        light.showAnimationFrame(light.sparkleAnimation)
+        pause(500)
+        light.setAll(0xffff00)
+    } else {
+        light.clear()
+    }
 })
