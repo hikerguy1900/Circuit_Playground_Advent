52db64088a185d3b1f3305e13dae02bd277a43b5
hikerguy1900 <ericb@piap.com>
2020-12-02 19:22:59 -0800

    Test of template code.

--- README.md
+++ README.md
@@ -1,6 +1,37 @@
-### @activities true
 # Advent
 ## Introduction
+
+```template
+let button_count = 0
+input.buttonA.onEvent(ButtonEvent.Click, function () {
+    button_count += 1
+    if (button_count > 4) {
+        button_count = 0
+    }
+})
+forever(function () {
+    if (button_count == 0) {
+        light.showRing(
+        `black black black purple purple black black black black black`
+        )
+    } else if (button_count == 0) {
+        light.showRing(
+        `purple purple black purple purple black black black black black`
+        )
+    } else if (button_count == 0) {
+        light.showRing(
+        `purple purple black purple purple black black black pink pink`
+        )
+    } else if (button_count == 0) {
+        light.showRing(
+        `purple purple black purple purple purple purple black pink pink`
+        )
+    } else {
+        light.clear()
+    }
+})
+```
+
 
 ## Activity 1: Varaibles
 ### Step
--- main.ts
+++ main.ts
@@ -1,41 +1,27 @@
-input.buttonA.onEvent(ButtonEvent.LongClick, function () {
-    music.setVolume(255)
-    music.playMelody("A F E F D G E F ", 120)
-    music.setVolume(0)
-})
+let button_count = 0
 input.buttonA.onEvent(ButtonEvent.Click, function () {
     button_count += 1
-    console.logValue("button_count", button_count)
-    if (button_count >= 6) {
+    if (button_count > 4) {
         button_count = 0
     }
 })
-let button_count = 0
-button_count = 0
-music.stopAllSounds()
-music.setVolume(0)
-console.log("\"Start\"")
 forever(function () {
-    if (button_count == 1) {
+    if (button_count == 0) {
         light.showRing(
-        `black black black pink pink black black black black black`
+        `black black black purple purple black black black black black`
         )
-    } else if (button_count == 2) {
+    } else if (button_count == 0) {
         light.showRing(
-        `pink pink black pink pink black black black black black`
+        `purple purple black purple purple black black black black black`
         )
-    } else if (button_count == 3) {
+    } else if (button_count == 0) {
         light.showRing(
-        `pink pink black pink pink black black black purple purple`
+        `purple purple black purple purple black black black pink pink`
         )
-    } else if (button_count == 4) {
+    } else if (button_count == 0) {
         light.showRing(
-        `pink pink black pink pink pink pink black purple purple`
+        `purple purple black purple purple purple purple black pink pink`
         )
-    } else if (button_count == 5) {
-        light.showAnimationFrame(light.sparkleAnimation)
-        pause(500)
-        light.setAll(0xffff00)
     } else {
         light.clear()
     }
